fcc4e0dfa3d6af8c685b4686553d75b2d2d46510
“Yourhong520 <754481882@qq.com>
2023-05-09 16:22:41 +0800

    .

---
+++ super-monkey-parent/src/main/java/com/woniu/Test.java
@@ -0,0 +1,14 @@
+package com.woniu;
+
+/**
+ * @Title: Test
+ * @Author linminwei
+ * @Package com.woniu
+ * @Date 2023/5/9 16:22
+ * @description: 测试
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试推送");
+    }
+}
